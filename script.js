d8e53b3b8f947af611f70e4e64057759817de724
Mario <boing3000@gmx.de>
2023-04-01 20:18:37 +0200

    style and cookie update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Global script variables
     var correct_answers = 0;
@@ -9,12 +9,61 @@ $(document).ready(function() {
     var clicked_subject = null;
     var clicked_verb = null;
 
-    $.getJSON(json_file, function(data) {
+    function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Lax";
+    }
+
+    function getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    function checkCookie() {
+        let fromlang = getCookie("fromlang");
+        let tolang = getCookie("tolang");
+        if (fromlang != "" || tolang != "") {
+            console.log("Cookie was already set: " + fromlang + " - " + tolang);
+        } else {
+            setCookie("fromlang", "german", 365);
+            setCookie("tolang", "english", 365);
+            alert("Cookie now set: german - english");
+        }
+    }
+
+    $(document).on('click', '.language_select_button', function () {
+        const language_split = $(this).text().split("-");
+        const language_split_fromlang = language_split[0].toLowerCase();
+        const language_split_tolang = language_split[1].toLowerCase();
+        // alert(language_split_fromlang + language_split_tolang);
+        setCookie("fromlang", language_split_fromlang, 365);
+        setCookie("tolang", language_split_tolang, 365);
+        alert("Cookie was changed to the values: " + language_split_fromlang + "-" + language_split_tolang);
+    });
+
+    // Always check if a Cookie is set and set the global vars
+    checkCookie();
+    var fromlang_cookie = getCookie("fromlang");
+    var tolang_cookie = getCookie("tolang");
+    $('#language_select').html(fromlang_cookie.toUpperCase() + " - " + tolang_cookie.toUpperCase());
+
+    $.getJSON(json_file, function (data) {
         var dict = {};
-        for(var key in data) {
-            var value = data[key]["german"];
-            // console.log(value);
-            dict[data[key]["german"]] = data[key]["spanish"]
+        for (var key in data) {
+            dict[data[key][fromlang_cookie]] = data[key][tolang_cookie]
         }
         // console.log(dict);
         var correction_dict2 = dict;
@@ -22,22 +71,22 @@ $(document).ready(function() {
         function selectRandomKeys(obj) {
             const keys = Object.keys(obj);
             const randomKeys = [];
-          
+
             while (randomKeys.length < max_count) {
-              const randomIndex = Math.floor(Math.random() * keys.length);
-              const randomKey = keys[randomIndex];
-          
-              if (!randomKeys.includes(randomKey)) {
-                randomKeys.push(randomKey);
-              }
+                const randomIndex = Math.floor(Math.random() * keys.length);
+                const randomKey = keys[randomIndex];
+
+                if (!randomKeys.includes(randomKey)) {
+                    randomKeys.push(randomKey);
+                }
             }
-          
+
             const randomObj = {};
-          
+
             randomKeys.forEach(key => {
-              randomObj[key] = obj[key];
+                randomObj[key] = obj[key];
             });
-          
+
             return randomObj;
         }
         const randomObj = selectRandomKeys(correction_dict2);
@@ -74,81 +123,81 @@ $(document).ready(function() {
         var shuffled_obj = shuffleObject(shuffled_dict);
         console.log(shuffled_obj);
 
-        $.each(shuffled_obj, function(subject, verb) {
-        $('#output_table').append('<tr><td><input type="submit" class="button subject-button" value="'+ subject +'"></td><td><input type="submit" class="button conjugation-button" value="'+ verb +'"></td></tr>');  
+        $.each(shuffled_obj, function (subject, verb) {
+            $('#output_table').append('<tr><td><input type="submit" class="button subject-button" value="' + subject + '"></td><td><input type="submit" class="button conjugation-button" value="' + verb + '"></td></tr>');
         });
     });
 
-    $(document).on('click', '.subject-button', function() {
+    $(document).on('click', '.subject-button', function () {
         selected_subject = $(this).val();
         clicked_subject = $(this);
-        $("#wrong_div").css("display","none");
-        $("#correct_div").css("display","none");
-        if(selected_verb === null) {
-        $('.subject-button').removeClass('selected');
-        $('.conjugation-button').removeClass('selected');
-        $(this).addClass('selected');
+        $("#wrong_div").css("display", "none");
+        $("#correct_div").css("display", "none");
+        if (selected_verb === null) {
+            $('.subject-button').removeClass('selected');
+            $('.conjugation-button').removeClass('selected');
+            $(this).addClass('selected');
         } else {
-        if(selected_verb === correction_dict[selected_subject]) {
-            console.log("Answer is correct");
-            clicked_subject.addClass('grayed-out');
-            clicked_verb.addClass('grayed-out');
-            clicked_subject.attr("disabled", "disabled");
-            clicked_verb.attr("disabled", "disabled");
-            clicked_subject.removeClass('subject-button');
-            clicked_verb.removeClass('conjugation-button');
-            correct_answers++;
-            $("#correct_div").css("display","block");
-            if(correct_answers === max_count) {
-                $("#all_correct_div").css("display","block");
+            if (selected_verb === correction_dict[selected_subject]) {
+                console.log("Answer is correct");
+                clicked_subject.addClass('grayed-out');
+                clicked_verb.addClass('grayed-out');
+                clicked_subject.attr("disabled", "disabled");
+                clicked_verb.attr("disabled", "disabled");
+                clicked_subject.removeClass('subject-button');
+                clicked_verb.removeClass('conjugation-button');
+                correct_answers++;
+                $("#correct_div").css("display", "block");
+                if (correct_answers === max_count) {
+                    $("#all_correct_div").css("display", "block");
+                }
+            } else {
+                $("#wrong_div").css("display", "block");
             }
-        } else {
-            $("#wrong_div").css("display","block");
-        }
-        $('.subject-button').removeClass('selected');
-        $('.conjugation-button').removeClass('selected');
-        selected_subject = null;
-        selected_verb = null;
-        clicked_subject = null;
-        clicked_verb = null;
+            $('.subject-button').removeClass('selected');
+            $('.conjugation-button').removeClass('selected');
+            selected_subject = null;
+            selected_verb = null;
+            clicked_subject = null;
+            clicked_verb = null;
         }
     });
 
-    $(document).on('click', '.conjugation-button', function() {
+    $(document).on('click', '.conjugation-button', function () {
         selected_verb = $(this).val();
         clicked_verb = $(this);
-        $("#wrong_div").css("display","none");
-        $("#correct_div").css("display","none");
-        if(selected_subject === null) {
-        $('.subject-button').removeClass('selected');
-        $('.conjugation-button').removeClass('selected');
-        $(this).addClass('selected');
+        $("#wrong_div").css("display", "none");
+        $("#correct_div").css("display", "none");
+        if (selected_subject === null) {
+            $('.subject-button').removeClass('selected');
+            $('.conjugation-button').removeClass('selected');
+            $(this).addClass('selected');
         } else {
-        if(selected_verb === correction_dict[selected_subject]) {
-            clicked_subject.addClass('grayed-out');
-            clicked_verb.addClass('grayed-out');
-            clicked_subject.attr("disabled", "disabled");
-            clicked_verb.attr("disabled", "disabled");
-            clicked_subject.removeClass('subject-button');
-            clicked_verb.removeClass('conjugation-button');
-            correct_answers++;
-            $("#correct_div").css("display","block");
-            if(correct_answers === max_count) {
-                $("#all_correct_div").css("display","block");
+            if (selected_verb === correction_dict[selected_subject]) {
+                clicked_subject.addClass('grayed-out');
+                clicked_verb.addClass('grayed-out');
+                clicked_subject.attr("disabled", "disabled");
+                clicked_verb.attr("disabled", "disabled");
+                clicked_subject.removeClass('subject-button');
+                clicked_verb.removeClass('conjugation-button');
+                correct_answers++;
+                $("#correct_div").css("display", "block");
+                if (correct_answers === max_count) {
+                    $("#all_correct_div").css("display", "block");
+                }
+            } else {
+                $("#wrong_div").css("display", "block");
             }
-        } else {
-            $("#wrong_div").css("display","block");
-        }
-        $('.subject-button').removeClass('selected');
-        $('.conjugation-button').removeClass('selected');
-        selected_subject = null;
-        selected_verb = null;
-        clicked_subject = null;
-        clicked_verb = null;
+            $('.subject-button').removeClass('selected');
+            $('.conjugation-button').removeClass('selected');
+            selected_subject = null;
+            selected_verb = null;
+            clicked_subject = null;
+            clicked_verb = null;
         }
     });
 
-    $(document).on('click', '#next_verb', function() {
+    $(document).on('click', '#next_verb', function () {
         location.reload();
     });
 
