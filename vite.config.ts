70a2a09e3fd556a5a5ed2395b4e4cdfe1227d1f6
Mario <boing3000@gmx.de>
2026-02-08 16:43:49 +0100

    RENEW THE VITE BASE

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: './',   // build directly to current folder
-    emptyOutDir: false, // don’t delete src/ or public/
-  },
-  base: './',       // relative paths for assets
+  base: './', // relative paths work locally AND on GitHub Pages
 })
